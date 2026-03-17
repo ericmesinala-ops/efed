02cd5bd63a60f156e190c9bdb69ab7050c3a375a
ericmesinala-ops <ericmesina_la@yahoo.com>
2026-03-17 01:23:23 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,9 @@
-const CACHE = 'efed-hub-v3';
-const STATIC = [
-  '/',
-  '/index.html',
-  '/auth.html',
-  '/efed-hub-icon.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
-  );
-});
-
+// v4 - network only, no caching
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('fetch', e => {
-  // Only cache same-origin GET requests
-  if (e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)) return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('fetch', () => {});
